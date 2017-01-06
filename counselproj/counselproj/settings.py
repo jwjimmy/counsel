59e0138c829af9b425808ffb6145e7b2259075c2
James W <james.wu@alumni.olin.edu>
2017-01-06 18:02:05 -0500

    add the herokuapp to allowed hosts

--- counselproj/counselproj/settings.py
+++ counselproj/counselproj/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'lf#i!&gjzsk*)9oss*cv1jtaqu$$22i%=x-(cvmq3@rx*^58@2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fathomless-castle-35327.herokuapp.com']
 
 
 # Application definition
