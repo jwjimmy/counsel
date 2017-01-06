03da61f2d418ea7b146800fe23a38c2fdef171cb
James W <james.wu@alumni.olin.edu>
2017-01-06 17:41:43 -0500

    fix staticurl?

--- counselproj/counselproj/settings.py
+++ counselproj/counselproj/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'counselapp/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
