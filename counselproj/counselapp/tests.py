36e803f679f8b098039525a3ee045631b2573366
James W <james.wu@alumni.olin.edu>
2017-02-16 00:36:41 -0500

    use user agent parsing to detect request type

--- counselproj/counselapp/tests.py
+++ counselproj/counselapp/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, RequestFactory
 from counselapp.utils import get_serialized_meta
+from counselapp.utils import RequestType, get_request_type
 import json
 
 # Create your tests here.
@@ -11,3 +12,26 @@ class RequestMetaTest(TestCase):
 	def test_serialize_meta(self):
 		request = self.factory.get('/requests/passive')
 		get_serialized_meta(request.META)
+
+class UserAgentTest(TestCase):
+	def setUp(self):
+		pass
+
+	def test_iphone(self):
+		meta = {}
+		meta["HTTP_USER_AGENT"] = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3"
+		meta["HTTP_REFERER"] = "bob.loblaw.com/hello"
+		request_type = get_request_type(meta)
+		self.assertEqual(request_type, RequestType.DIRECT)
+
+	def test_gmail(self):
+		meta = {}
+		meta["HTTP_USER_AGENT"] = "Mozilla/5.0 (Windows NT 7.1; rv:16.0) Gecko Firefox/10.0 (via ggpht.com GoogleImageProxy)"
+		request_type = get_request_type(meta)
+		self.assertEqual(request_type, RequestType.GMAIL)
+
+	def test_github(self):
+		meta = {}
+		meta["HTTP_USER_AGENT"] = "github-camo (n644qup0)"
+		request_type = get_request_type(meta)
+		self.assertEqual(request_type, RequestType.GITHUB)
--- counselproj/counselapp/utils.py
+++ counselproj/counselapp/utils.py
@@ -1,5 +1,29 @@
 import json
 import re
+from ua_parser import user_agent_parser
+import logging
+logger = logging.getLogger('django.request')
+
+class RequestType:
+	UNKNOWN = 0
+	DIRECT = 1
+	GMAIL = 2
+	GITHUB = 3
+
+def get_request_type(meta):
+	ua_string = meta.get("HTTP_USER_AGENT", None)
+	http_referer = meta.get("HTTP_REFERER", None)
+	ua_dict = user_agent_parser.ParseUserAgent(ua_string)
+
+	if http_referer == None and ua_dict['family'] == 'GmailImageProxy':
+		return RequestType.GMAIL
+	elif http_referer == None and ua_dict['family'] == 'Other' and "github-camo" in ua_string:
+		return RequestType.GITHUB
+	elif http_referer:
+		return RequestType.DIRECT
+	else:
+		logger.info("Could not get RequestType of " + ua_string)
+		return RequestType.UNKNOWN
 
 def quote_objects(meta_str):
 	return re.sub(r".(?<=<)(.*?)(?=>).", r'"\1"', meta_str)
